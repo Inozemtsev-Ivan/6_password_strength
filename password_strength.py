0577e4398c95b0604e2de67e4a7322a226e1852e
inozemtsev-ivan <inozemtsev.il@gmail.com>
2017-11-26 00:12:29 +0300

    Specify boilerplate for required functions accordingly to list of passwords' best-practices.

--- password_strength.py
+++ password_strength.py
@@ -1,3 +1,49 @@
+def check_case_sensitivity():
+    # the use of both upper-case and lower-case letters (case sensitivity)
+    pass
+
+
+def check_digits():
+    # inclusion of one or more numerical digits
+    pass
+
+
+def check_special_chars():
+    # inclusion of special characters, such as @, #, $
+    pass
+
+
+def load_blacklist():
+    pass
+
+
+def check_blacklist():
+    # prohibition of words found in a password blacklist
+    pass
+
+
+def load_personal_info():
+    pass
+
+
+def check_personal_info():
+    # prohibition of words found in the user's personal information
+    pass
+
+
+def load_company_info():
+    pass
+
+
+def check_company_info():
+    # prohibition of use of company name or an abbreviation
+    pass
+
+
+def check_formats():
+    # prohibition of passwords that match the format of calendar dates,
+    # license plate numbers, telephone numbers, or other common numbers
+    pass
 
 
 def get_password_strength(password):
